b442f1ef226b2f2425a975a625b528cdc3bbe58f
DonTizi <66443081+DonTizi@users.noreply.github.com>
2024-01-09 16:11:40 -0500

    Update indexing.py

--- indexing.py
+++ indexing.py
@@ -12,11 +12,11 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 
 # Set your Pinecone API key and environment
-pinecone_api = "cd4f4df8-d7ee-405e-86de-1f2c37485a14"
-pinecone_env = "us-west4-gcp-free"
+pinecone_api = "APIKEY"
+pinecone_env = "ENV"
 
 # Set your OpenAI API key
-openai_api = "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"
+openai_api = "APIKEY"
 # Initialize Pinecone
 pinecone.init(api_key=pinecone_api, environment=pinecone_env)
 
@@ -27,7 +27,7 @@ if index_name not in pinecone.list_indexes():
     pinecone.create_index(name=index_name, metric="cosine", dimension=1536)
 
 # Load the documents using TextLoader
-loader = TextLoader("/Users/dontizi/Documents/Rewind/Database_management/all_texts.txt")
+loader = TextLoader("./all_texts.txt") #Replace it with your path
 documents = loader.load()
 # Split the documents into chunks using CharacterTextSplitter
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -40,7 +40,7 @@ embeddings = OpenAIEmbeddings(api_key=openai_api)
 docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
 # Perform similarity search
-query = sys.argv[1] if len(sys.argv) > 1 else "quels musiques jecoute quotidiennement sur apple music?"
+query = sys.argv[1] if len(sys.argv) > 1 else "what music I played on apple music?"
 docs = docsearch.similarity_search(query)
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
